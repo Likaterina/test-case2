c8d9f70ecf55a122113394c4997b299f14cee37c
Likaterina <linainadelira@gmail.com>
2019-12-24 17:11:44 +0200

    without me

--- socket.js
+++ socket.js
@@ -1,35 +1,51 @@
 const jwt = require('jsonwebtoken')
 
 let users = {}
+const SALTjwt = "reactcal"
 
 const socket = io => {
-    io.on("connection", socket => {
-        console.log("connected")
-        socket.on("online", user => {
-            console.log(user)
-            if (user) {
-                console.log(socket.id)
-                socket.id = user.userId
-                if (users[user.userId] == undefined) {
-                    users[user.userId] = { ...user, online: true }
-                }
-                else {
-                    users[user.userId].online = true
-                }
-            }
-            console.log(users)
-        })
+    // middleware
+    io.use((socket, next) => {
+        const { token } = socket.handshake.query;
+        if (jwt.verify(token,SALTjwt)) {
+            return next();
+        }
+        return next(new Error('authentication error'));
+    });
 
-        socket.broadcast.emit('broadcast', { description: users })
+    io.on("connection", async socket => {
+        const { token } = socket.handshake.query;
+        const user = await bd.find();
+
+        if (!user) {
+            // socket.logout()
+        }
+        // socket.on("online", user => {
+        //     console.log(user)
+        //     if (user) {
+        //         console.log(socket.id)
+        //         socket.id = user.userId
+        //         if (users[user.userId] == undefined) {
+        //             users[user.userId] = { ...user, online: true }
+        //         }
+        //         else {
+        //             users[user.userId].online = true
+        //         }
+        //     }
+        //     console.log(users)
+        // })
+        console.log(`Hi, we are ${users}`)
+        socket.broadcast.emit('broadcast', users )
+        socket.emit('broadcast', users )
 
         socket.on("chat message", msg => {
             io.emit("chat message", msg)
         })
-
+        console.log(users)
         socket.on("disconnect", () => {
-                socket.broadcast.emit('broadcast', { users })
-                users[user.userId].online = false
-                console.log("disconnected")
+                // socket.broadcast.emit('broadcast', users )
+                // users[user.userId].online = false
+                // console.log("disconnected")
         })
 
     })
